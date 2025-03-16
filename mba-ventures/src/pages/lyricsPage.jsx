52ae8fadbee3cb7b2e61c0360d5467272100e20a
Daniel G. Amoateng <f005cm3@dartmouth.edu>
2025-03-16 01:04:45 -0400

    This should deploy now

---
+++ mba-ventures/src/pages/lyricsPage.jsx
@@ -0,0 +1,11 @@
+function LyricsPage() {
+  return (
+    <div className="container">
+      <h1>Lyrics Page</h1>
+      <p>Here, users can post and view lyrics.</p>
+      {/* Later: Fetch and display lyrics from Firebase */}
+    </div>
+  );
+}
+
+export default LyricsPage;
