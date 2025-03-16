48745016aaf5d4c06eadc878d1c278b007490fd9
f005cm3 <f005cm3@dartmouth.edu>
2025-03-15 22:48:40 -0400

    General structure ready to go.

---
+++ mba-venture/src/pages/landingPage.jsx
@@ -0,0 +1,18 @@
+import { Link } from "react-router-dom";
+
+function LandingPage() {
+  return (
+    <div className="container">
+      <h1>Welcome to My Website</h1>
+      <p>Explore our content:</p>
+      <ul>
+        <li><Link to="/lyrics">Lyrics</Link></li>
+        <li><Link to="/books">Books</Link></li>
+        <li><Link to="/videos">Videos</Link></li>
+        <li><Link to="/print-press">Print Press</Link></li>
+      </ul>
+    </div>
+  );
+}
+
+export default LandingPage;
